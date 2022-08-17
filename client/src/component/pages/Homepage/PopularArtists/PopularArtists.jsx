1fb8d7a31946c9b25a297599ebdce7a64de0deb9
Shane Castro <shaneh026@gmail.com>
2022-08-16 20:33:27 -0400

    added infowrapper

--- client/src/component/pages/Homepage/PopularArtists/PopularArtists.jsx
+++ client/src/component/pages/Homepage/PopularArtists/PopularArtists.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import styled from "styled-components";
+import { GrCaretPrevious, GrCaretNext } from "react-icons/gr";
 
 const PopularArtists = () => {
   return (
     <Container>
       <InfoWrapper>
-        <h2>Popular Artists</h2>
-        <div className="prev-next"></div>
+        <div>
+          <h2>Popular Artists</h2>
+        </div>
+        <div className="prev-next">
+          <GrCaretPrevious size={30} />
+          <GrCaretNext size={30} />
+        </div>
       </InfoWrapper>
       <CardWrapper></CardWrapper>
     </Container>
   );
 };
 
-const Container = styled.div``;
-const InfoWrapper = styled.div``;
+const Container = styled.div`
+  margin-top: 20px;
+  border: 1px solid red;
+  min-height: 35vh;
+`;
+const InfoWrapper = styled.div`
+  padding: 15px;
+  width: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid red;
+
+  .prev-next {
+    display: flex;
+  }
+`;
 const CardWrapper = styled.div``;
 
 export default PopularArtists;
