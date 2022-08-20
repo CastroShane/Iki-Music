583a8e7d129fd46df49c061f8e65d133a841197e
Shane Castro <shaneh026@gmail.com>
2022-08-19 20:29:47 -0400

    added signup functionality

--- client/src/component/pages/AccountPage/SignUpForm.jsx
+++ client/src/component/pages/AccountPage/SignUpForm.jsx
@@ -1,5 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { CurrentUserContext } from "../../context/CurrentUser";
 import { AccountContext } from "./AccountContext";
+import { useQuery } from "react-query";
+
 import {
   BoldLink,
   BoxContainer,
@@ -10,20 +13,111 @@ import {
 } from "./Common";
 import Marginer from "./Margin";
 
+export const defaultFormFields = {
+  fullName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const SignUpForm = () => {
   const { switchToSignIn } = useContext(AccountContext);
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { fullName, email, password, confirmPassword } = formFields;
+  const [error, setError] = useState(null);
+  const [errorText, setErrorText] = useState("");
+  const { setCurrentUser } = useContext(CurrentUserContext);
 
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError(true);
+      setErrorText("Passwords do not match");
+      return;
+    }
+
+    const response = await fetch("/account", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        fullName,
+        email: email.toLowerCase(),
+        password,
+      }),
+    });
+    const data = await response.json();
+    const newUserData = data.data;
+
+    if (!newUserData) {
+      setError(true);
+      setErrorText(data.message);
+      return;
+    } else {
+      setError(false);
+      resetFormFields();
+      setErrorText(data.message);
+      setCurrentUser(newUserData);
+      switchToSignIn();
+    }
+  };
+
+  //Store value to formFields State
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields({ ...formFields, [name]: value });
+    setError(false);
+  };
+  console.log(error);
+  console.log(formFields);
   return (
     <BoxContainer>
-      <FormContainer>
-        <Input type="text" placeholder="Full Name" required></Input>
-        <Input type="email" placeholder="Email" required></Input>
-        <Input type="password" placeholder="Password" required></Input>
-        <Input type="password" placeholder="Confirm Password" required></Input>
+      <FormContainer onSubmit={handleSubmit} id="signup-form">
+        <Input
+          type="text"
+          placeholder="Full Name"
+          name="fullName"
+          onChange={handleChange}
+          value={fullName}
+          required
+        ></Input>
+        <Input
+          type="email"
+          placeholder="Email"
+          name="email"
+          onChange={handleChange}
+          value={email}
+          required
+        ></Input>
+        <Input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleChange}
+          value={password}
+          required
+        ></Input>
+        <Input
+          type="password"
+          placeholder="Confirm Password"
+          name="confirmPassword"
+          onChange={handleChange}
+          value={confirmPassword}
+          required
+        ></Input>
       </FormContainer>
       <Marginer direction={"vertical"} margin={10} />
-
-      <SubmitButton type="submit">Signup</SubmitButton>
+      {error && <MutedLink style={{ color: "red" }}>{errorText}</MutedLink>}
+      {!error && <MutedLink style={{ color: "green" }}>{errorText}</MutedLink>}
+      <SubmitButton type="submit" form="signup-form">
+        Signup
+      </SubmitButton>
       <Marginer direction={"vertical"} margin="1em" />
       <MutedLink href="#">
         Already have an account?
