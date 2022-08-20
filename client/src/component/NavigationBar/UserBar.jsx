0029bc2ccdd21a397846ec595ef7c65dc7e9950d
Shane Castro <shaneh026@gmail.com>
2022-08-19 22:27:39 -0400

    finished redirections when signing in

---
+++ client/src/component/NavigationBar/UserBar.jsx
@@ -0,0 +1,67 @@
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { CurrentUserContext } from "../context/CurrentUserContext";
+import styled from "styled-components";
+import { GoDash } from "react-icons/go";
+
+const UserBar = () => {
+  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
+  const { fullName } = currentUser;
+
+  //Redirects to homepage whenever someone signs-out
+  const history = useHistory();
+  const routeChange = () => {
+    let path = `/`;
+    history.push(path);
+  };
+
+  //Resets all the state whenever user signs out
+  const handleSignOut = (e) => {
+    e.preventDefault();
+    setCurrentUser({});
+    routeChange();
+  };
+
+  return (
+    <Wrapper>
+      <StyledSpan>{`Hello, ${fullName.split(" ")[0]}`}</StyledSpan>
+      <Dash />
+      <SignOutBtn onClick={(e) => handleSignOut(e)}>Sign Out</SignOutBtn>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`;
+const StyledImg = styled.img`
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  margin: 0 10px;
+`;
+
+const StyledSpan = styled.span`
+  color: var(--stronger-font-color);
+  font-family: var(--font-heading);
+  font-size: 18px;
+`;
+
+const SignOutBtn = styled.button`
+  margin: 5px;
+  text-decoration: none;
+  color: var(--stronger-font-color);
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+`;
+
+const Dash = styled(GoDash)`
+  margin: 0 5px;
+`;
+
+export default UserBar;
--- client/src/component/context/CurrentUserContext.jsx
+++ client/src/component/context/CurrentUserContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from "react";
 
 export const CurrentUserContext = createContext();
 export const CurrentUserContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState();
+  const [currentUser, setCurrentUser] = useState({});
   const [error, setError] = useState(false);
 
   return (
