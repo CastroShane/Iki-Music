926ff2627644b451b4e37677f5c7157f23a57542
Shane Castro <shaneh026@gmail.com>
2022-08-25 15:00:32 -0400

    started loading state in homepage

---
+++ client/src/component/LoadingComponents/LoadingCards.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import cardImage from "./assets/loadingcard.jpg";
+const LoadingCards = () => {
+  return (
+    <motion.div
+      style={{ display: "flex" }}
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      layout
+    >
+      <StyledLink>
+        <img src={cardImage} alt="img1" />
+        <p>Name</p>
+      </StyledLink>
+      <StyledLink>
+        <img src={cardImage} alt="img2" />
+        <p>Name</p>
+      </StyledLink>
+      <StyledLink>
+        <img src={cardImage} alt="img3" />
+        <p>Name</p>
+      </StyledLink>
+      <StyledLink>
+        <img src={cardImage} alt="img4" />
+        <p>Name</p>
+      </StyledLink>
+      <StyledLink>
+        <img src={cardImage} alt="img5" />
+        <p>Name</p>
+      </StyledLink>
+      <StyledLink>
+        <img src={cardImage} alt="img6" />
+        <p>Name</p>
+      </StyledLink>
+      <StyledLink>
+        <img src={cardImage} alt="img7" />
+        <p>Name</p>
+      </StyledLink>
+      <StyledLink>
+        <img src={cardImage} alt="img8" />
+        <p>Name</p>
+      </StyledLink>
+      <StyledLink>
+        <img src={cardImage} alt="img9" />
+        <p>Name</p>
+      </StyledLink>
+      <StyledLink>
+        <img src={cardImage} alt="img10" />
+        <p>Name</p>
+      </StyledLink>
+      );
+    </motion.div>
+  );
+};
+
+const StyledLink = styled(Link)`
+  width: 150px;
+  margin: 15px 20px;
+  text-decoration: none;
+  transition: transform 0.2s ease-out;
+
+  &:hover {
+    cursor: pointer;
+    transform: translate(2px, -5px);
+  }
+
+  img {
+    width: 150px;
+    height: 150px;
+  }
+`;
+
+export default LoadingCards;
