e6243ca2f8dc67a50c3a10e3e5c921afdc4be989
Shane Castro <mr.shanecastro@gmail.com>
2022-11-25 07:19:13 +0800

    small changes

--- server/server.js
+++ server/server.js
@@ -25,7 +25,15 @@ const {
 
 app
   .use(function (req, res, next) {
-    res.header("access-Control-allow-origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "PUT, POST, GET, DELETE, OPTIONS"
+    );
     next();
   })
   .use(morgan("tiny"))
